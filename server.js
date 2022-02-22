07a76ba08bb06eedf649e829f9301881dfca4d74
mikelun <erin788@mail.ru>
2022-02-23 00:08:27 +0800

    1

--- server.js
+++ server.js
@@ -65,6 +65,6 @@ io.on('connection', function (socket) {
 
 });
 
-server.listen(process.env.PORT || 3000, function () {
+server.listen(80, function () {
   console.log(`Listening on ${server.address().port}`);
 });
