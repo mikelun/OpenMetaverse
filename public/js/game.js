ccd981c104402fc9b8a8af3f3cbc778665afe37b
mikelun <erin788@mail.ru>
2022-02-26 01:10:54 +0800

    added WebRTC

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,6 @@
+const localVideoComponent = document.getElementById('local-video')
+const remoteVideoComponent = document.getElementById('remote-video')
+
 var config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
@@ -20,6 +23,67 @@ var config = {
 
 // SOCKET
 var socket;
+
+
+////////////////////
+//**** Audio  ****//
+
+// Free public STUN servers provided by Google.
+const iceServers = {
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' },
+    { urls: 'stun:stun1.l.google.com:19302' },
+    { urls: 'stun:stun2.l.google.com:19302' },
+    { urls: 'stun:stun3.l.google.com:19302' },
+    { urls: 'stun:stun4.l.google.com:19302' },
+  ],
+}
+
+const mediaConstraints = {
+  audio: true,
+  video: false,
+}
+let localStream
+let remoteStream
+let isRoomCreator
+let rtcPeerConnection // Connection between the local device and the remote peer.
+let roomId
+
+
+
+
+// FUNCTIONS ==================================================================
+
+function joinRoom(room) {
+  if (room === '') {
+    alert('Please type a room ID')
+  } else {
+    roomId = room
+    socket.emit('join', room)
+    //showVideoConference()
+  }
+}
+
+function showVideoConference() {
+  roomSelectionContainer.style = 'display: none'
+  videoChatContainer.style = 'display: block'
+}
+
+async function setLocalStream(mediaConstraints) {
+  let stream
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(mediaConstraints)
+  } catch (error) {
+    console.error('Could not get user media', error)
+  }
+
+  localStream = stream
+  localVideoComponent.srcObject = stream
+}
+
+
+///////////////////
+
 
 // TEXTS
 var playerName;
@@ -50,7 +114,7 @@ function editButtonClass(target, bool) {
     target.innerText = "Mute";
     return classList.add("enable-btn");
   }
-  
+
   target.innerText = "Unmute";
   classList.add("disable-btn");
 }
@@ -76,6 +140,7 @@ var keyUp, keyDown, keyLeft, keyRight;
 var map;
 
 function preload() {
+
   this.load.spritesheet(
     "characters",
     "assets/characterSprite.png",
@@ -100,23 +165,24 @@ function preload() {
 }
 
 function create() {
+
   map = this.add.image(526, 495, 'map');
   map.setScale(2.1);
 
   // ANIMS
   const anims = this.anims;
-    anims.create({
-      key: "player-walk",
-      frames: anims.generateFrameNumbers("characters", { start: 46, end: 49 }),
-      frameRate: 8,
-      repeat: -1
-    });
-    anims.create({
-      key: "player-walk-back",
-      frames: anims.generateFrameNumbers("characters", { start: 65, end: 68 }),
-      frameRate: 8,
-      repeat: -1
-    });
+  anims.create({
+    key: "player-walk",
+    frames: anims.generateFrameNumbers("characters", { start: 46, end: 49 }),
+    frameRate: 8,
+    repeat: -1
+  });
+  anims.create({
+    key: "player-walk-back",
+    frames: anims.generateFrameNumbers("characters", { start: 65, end: 68 }),
+    frameRate: 8,
+    repeat: -1
+  });
 
 
 
@@ -153,10 +219,131 @@ function create() {
   });
 
 
+  // SOCKET EVENT CALLBACKS =====================================================
+  socket.on('room_created', async () => {
+    console.log('Socket event callback: room_created')
+
+    await setLocalStream(mediaConstraints)
+    isRoomCreator = true
+  })
+
+  socket.on('room_joined', async () => {
+    console.log('Socket event callback: room_joined')
+
+    await setLocalStream(mediaConstraints)
+    socket.emit('start_call', roomId)
+  })
+
+  socket.on('full_room', () => {
+    console.log('Socket event callback: full_room')
+
+    alert('The room is full, please try another one')
+  })
+
+
+  // SOCKET STREAM AUDIO EVENT CALLBACKS =====================================================
+  socket.on('start_call', async () => {
+    console.log('Socket event callback: start_call')
+
+    if (isRoomCreator) {
+      rtcPeerConnection = new RTCPeerConnection(iceServers)
+      addLocalTracks(rtcPeerConnection)
+      rtcPeerConnection.ontrack = setRemoteStream
+      rtcPeerConnection.onicecandidate = sendIceCandidate
+      await createOffer(rtcPeerConnection)
+    }
+  })
+
+  socket.on('webrtc_offer', async (event) => {
+    console.log('Socket event callback: webrtc_offer')
+
+    if (!isRoomCreator) {
+      rtcPeerConnection = new RTCPeerConnection(iceServers)
+      addLocalTracks(rtcPeerConnection)
+      rtcPeerConnection.ontrack = setRemoteStream
+      rtcPeerConnection.onicecandidate = sendIceCandidate
+      rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event))
+      await createAnswer(rtcPeerConnection)
+    }
+  })
+
+  socket.on('webrtc_answer', (event) => {
+    console.log('Socket event callback: webrtc_answer')
+
+    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event))
+  })
+
+  socket.on('webrtc_ice_candidate', (event) => {
+    console.log('Socket event callback: webrtc_ice_candidate')
+
+    // ICE candidate configuration.
+    var candidate = new RTCIceCandidate({
+      sdpMLineIndex: event.label,
+      candidate: event.candidate,
+    })
+    rtcPeerConnection.addIceCandidate(candidate)
+  })
+
+  // STREAM AUDIO FUNCTIONS ==================================================================
+  function addLocalTracks(rtcPeerConnection) {
+    localStream.getTracks().forEach((track) => {
+      rtcPeerConnection.addTrack(track, localStream)
+    })
+  }
+
+  async function createOffer(rtcPeerConnection) {
+    let sessionDescription
+    try {
+      sessionDescription = await rtcPeerConnection.createOffer()
+      rtcPeerConnection.setLocalDescription(sessionDescription)
+    } catch (error) {
+      console.error(error)
+    }
+
+    socket.emit('webrtc_offer', {
+      type: 'webrtc_offer',
+      sdp: sessionDescription,
+      roomId,
+    })
+  }
+
+  async function createAnswer(rtcPeerConnection) {
+    let sessionDescription
+    try {
+      sessionDescription = await rtcPeerConnection.createAnswer()
+      rtcPeerConnection.setLocalDescription(sessionDescription)
+    } catch (error) {
+      console.error(error)
+    }
+
+    socket.emit('webrtc_answer', {
+      type: 'webrtc_answer',
+      sdp: sessionDescription,
+      roomId,
+    })
+  }
+
+  function setRemoteStream(event) {
+    remoteVideoComponent.srcObject = event.streams[0]
+    remoteStream = event.stream
+  }
+
+  function sendIceCandidate(event) {
+    if (event.candidate) {
+      socket.emit('webrtc_ice_candidate', {
+        roomId,
+        label: event.candidate.sdpMLineIndex,
+        candidate: event.candidate.candidate,
+      })
+    }
+  }
+
+  joinRoom(100);
+
   // AUDIO
 
   // this.socket.emit("userInformation", userStatus);
-  
+
   // navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
   //   console.log("Microphone is on");
   //   var madiaRecorder = new MediaRecorder(stream);
@@ -173,15 +360,15 @@ function create() {
   //     var audioBlob = new Blob(audioChunks);
   //     audioChunks = [];
 
-      
+
   //     var fileReader = new FileReader();
   //     fileReader.readAsDataURL(audioBlob);
-  
+
   //     fileReader.onloadend = function () {
   //       if (!userStatus.mute) return;
   //     var base64String = fileReader.result;
   //     //console.log(base64String);
-      
+
   //     socket.emit("voice", base64String);
 
   //     };
@@ -203,7 +390,7 @@ function create() {
   //   var audio = new Audio(data);
   //   audio.play();
   // });
-    
+
 
 }
 
@@ -213,16 +400,16 @@ function emitUserInformation() {
 
 function addPlayer(self, playerInfo) {
   self.sprite = self.physics.add
-      .sprite(400, 400, "characters", 0)
-      .setSize(22, 33)
-      .setOffset(23, 27);
+    .sprite(400, 400, "characters", 0)
+    .setSize(22, 33)
+    .setOffset(23, 27);
 
   self.sprite.anims.play("player-walk-back");
   self.cameras.main.startFollow(self.sprite, true);
   self.cameras.main.setBounds(0, 0, 1000, 1000);
 
   // Change texts
-  playerName = self.add.text(self.sprite.x, self.sprite.y, "player", {fontSize: '20px', color: '#ffffff'});
+  playerName = self.add.text(self.sprite.x, self.sprite.y, "player", { fontSize: '20px', color: '#ffffff' });
 }
 
 playerNames = [];
@@ -230,7 +417,7 @@ playerNames = [];
 function addOtherPlayers(self, playerInfo) {
   console.log(playerInfo);
   const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, "characters", 0);
-  const otherPlayerName = self.add.text(playerInfo.x, playerInfo.y, playerInfo.account, {fontSize: '20px', color: '#ffffff'});
+  const otherPlayerName = self.add.text(playerInfo.x, playerInfo.y, playerInfo.account, { fontSize: '20px', color: '#ffffff' });
   otherPlayer.anims.play("player-walk");
   otherPlayer.playerId = playerInfo.playerId;
   self.otherPlayers.add(otherPlayer);
@@ -252,33 +439,33 @@ function update() {
     if (keyUp.isDown) {
       sprite.y -= 3;
       //this.sprite.rotation = -3.14;
-  }
-  if (keyDown.isDown) {
+    }
+    if (keyDown.isDown) {
       sprite.y += 3;
       //this.sprite.rotation = 0;
-  }
-  if (keyLeft.isDown) {
-    sprite.x -= 3;
-    //this.sprite.rotation = 3.14 / 2;
-    sprite.setFlipX(true);
-  }
-  if (keyRight.isDown) {
+    }
+    if (keyLeft.isDown) {
+      sprite.x -= 3;
+      //this.sprite.rotation = 3.14 / 2;
+      sprite.setFlipX(true);
+    }
+    if (keyRight.isDown) {
       sprite.x += 3;
       //this.sprite.rotation = -3.14 / 2;
       sprite.setFlipX(false);
-  }
+    }
 
-  if (keyLeft.isDown || keyRight.isDown || keyDown.isDown) {
+    if (keyLeft.isDown || keyRight.isDown || keyDown.isDown) {
       sprite.anims.play("player-walk", true);
     } else if (keyUp.isDown) {
       sprite.anims.play("player-walk-back", true);
     } else {
       sprite.anims.stop();
-  }
-  
-  
-  
-    
+    }
+
+
+
+
     // emit player movement
     var x = this.sprite.x;
     var y = this.sprite.y;
--- server.js
+++ server.js
@@ -65,6 +65,46 @@ io.on('connection', function (socket) {
     io.emit('disconnect', socket.id);
   });
 
+
+  // ADDED AUDIO
+  socket.on('join', (roomId) => {
+    const roomClients = io.sockets.adapter.rooms[roomId] || { length: 0 }
+    const numberOfClients = roomClients.length
+
+    // These events are emitted only to the sender socket.
+    if (numberOfClients == 0) {
+      console.log(`Creating room ${roomId} and emitting room_created socket event`)
+      socket.join(roomId)
+      socket.emit('room_created', roomId)
+    } else if (numberOfClients == 1) {
+      console.log(`Joining room ${roomId} and emitting room_joined socket event`)
+      socket.join(roomId)
+      socket.emit('room_joined', roomId)
+    } else {
+      console.log(`Can't join room ${roomId}, emitting full_room socket event`)
+      socket.emit('full_room', roomId)
+    }
+  })
+
+  // These events are emitted to all the sockets connected to the same room except the sender.
+  socket.on('start_call', (roomId) => {
+    console.log(`Broadcasting start_call event to peers in room ${roomId}`)
+    console.log()
+    socket.broadcast.to(roomId).emit('start_call')
+  })
+  socket.on('webrtc_offer', (event) => {
+    console.log(`Broadcasting webrtc_offer event to peers in room ${event.roomId}`)
+    socket.broadcast.to(event.roomId).emit('webrtc_offer', event.sdp)
+  })
+  socket.on('webrtc_answer', (event) => {
+    console.log(`Broadcasting webrtc_answer event to peers in room ${event.roomId}`)
+    socket.broadcast.to(event.roomId).emit('webrtc_answer', event.sdp)
+  })
+  socket.on('webrtc_ice_candidate', (event) => {
+    console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`)
+    socket.broadcast.to(event.roomId).emit('webrtc_ice_candidate', event)
+  })
+
 });
 
 server.listen(port, function () {
